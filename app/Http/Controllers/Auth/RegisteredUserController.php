875f6f94fed2f664a8cd9fea539b7a98721270ad
Felipe Kennedy <felipekennedyfvs2019@gmail.com>
2021-07-16 13:54:29 -0300

    Aula 04
    Permissões de usuário

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -43,7 +43,8 @@ class RegisteredUserController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
-        ]);
+        ])->givePermissionTo('user');
+        
 
         event(new Registered($user));
 
